d386d74e40a5ffdb34e8d01eef80b62bd53fbd18
Johno <john@mount-media-online.com>
2025-02-13 17:46:46 +0000

    catch inst, controller, models

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose')
-const dbUri = 'mongodb+srv://johnofisher01:<db_password>@databasetestjf1.ktdqvew.mongodb.net/book_register?retryWrites=true&w=majority&appName=DatabaseTestJf1'
+const dbUri = 'mongodb+srv://billy:12341234@databasetestjf1.ktdqvew.mongodb.net/book_register?retryWrites=true&w=majority&appName=DatabaseTestJf1'
 
 
 module.exports = () =>  { return mongoose.connect(dbUri)}
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = require ('express')
+const express = require("express");
 
-const app = express()
+//local import files
 
-app.listen(3000, ()=> {
-    console.log('server 3000')
-}).on, err => console.log('server ignition failed:\n', err)
+const connectDb = require("./db");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server 3000");
+}).on,
+  (err) => console.log("server ignition failed:\n", err);
+
+connectDb().then((data) => {
+  console.log("db connection successful ");
+})
+.catch(err => console.log('Error in connecting database\n:', err))
